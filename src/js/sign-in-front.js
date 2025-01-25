aee99db7efce8ce1d0c57f742149777170d64db8
rhim <yeqiu.jinwoo@gmail.com>
2025-01-26 04:56:31 +0800

    finalize sign-in/sign-up add alert

--- src/js/sign-in-front.js
+++ src/js/sign-in-front.js
@@ -12,9 +12,17 @@ const forgotAnchor = document.getElementById('forgot-anchor')
 
 const goBackBtn = document.getElementById('si-go-back-btn');
 const sendCodeBtn = document.getElementById('si-send-code-btn');
-
+ 
 const signInContainer = document.getElementById('si-login-input-container');
 const forgotPassContainer = document.getElementById('forgot-password-container');
+const siNewPasswordContainer = document.getElementById('si-changePass-container');
+
+const siGetEmailContainer = document.getElementById('si-get-email-container');
+const siSubmitCodeContainer = document.getElementById('si-submit-code-container');
+const siSubmitCodeBtn = document.getElementById('si-submit-code-btn');
+const siChangePassBtn = document.getElementById('si-newPass-btn');
+
+
 function isInputsFilled(array) {
     for (const input of array) {
         if (input.value === "") {
@@ -33,7 +41,64 @@ function capitalizeInput(inputElement) {
     });
 };
 
+
 capitalizeInput(studentIdInput)
+
+function toggleButtonState(input, button) {
+    input.addEventListener('input', () => {
+        if (input.value.trim() === '') {
+            button.disabled = true;
+        } 
+        else {
+            button.disabled = false;
+        }
+    });
+}
+const siGetEmail = document.getElementById('si-email');
+const siCode = document.getElementById('si-code');
+const siNewPass = document.getElementById('si-new-pass');
+
+toggleButtonState(siCode, siSubmitCodeBtn);
+toggleButtonState(siNewPass, siChangePassBtn);
+
+function validateEmail(event) {
+    const emailInput = event.target;
+    const email = emailInput.value;
+    
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    
+    const parentElement = document.querySelector('.label-err-container'); 
+    const existingError = parentElement.querySelector('.error-message');
+
+    if (existingError) {
+        existingError.remove();
+    }
+    
+    if (!emailRegex.test(email)) {
+        const errorMessage = document.createElement('span');
+        errorMessage.classList.add('text-sm', 'text-error', 'italic', 'font-medium', 'font-inter', 'error-message');
+        errorMessage.textContent = 'Enter a valid email.';
+        parentElement.appendChild(errorMessage);
+        sendCodeBtn.disabled = true;
+        
+    } 
+    else {
+        if(existingError) {
+            existingError.remove();
+            sendCodeBtn.disabled = false;
+        }
+    }
+}
+
+// function addEmailValidation(inputsArray) {
+//     inputsArray.forEach(input => {
+//         input.addEventListener('blur', validateEmail); 
+//     });
+// }
+siGetEmail.addEventListener('blur', validateEmail);
+siGetEmail.addEventListener('focus', validateEmail);
+siGetEmail.addEventListener('input', validateEmail);
+
 
 loginInputs.forEach(input => {
     input.addEventListener('input', () => {
@@ -84,4 +149,60 @@ goBackBtn.addEventListener('click', (event) => {
     signInContainer.classList.replace('hidden', 'flex');
     forgotPassContainer.classList.replace('flex', 'hidden');
 
+})
+
+
+sendCodeBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    
+
+
+    siGetEmailContainer.classList.replace('flex', 'hidden');
+    siSubmitCodeContainer.classList.replace('hidden', 'flex');
+})
+
+
+
+
+
+siSubmitCodeBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    siSubmitCodeContainer.classList.replace('flex', 'hidden');
+    siNewPasswordContainer.classList.replace('hidden', 'flex');   
+})
+
+
+siChangePassBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const loadingScreen = document.getElementById('loading-screen');
+
+    loadingScreen.classList.replace('hidden', 'flex');
+});
+// Loading Screen when the user want to send code to email 
+// sendCodeBtn.addEventListener('click', (event) => {
+//     event.preventDefault();
+
+//     const loadingScreen = document.getElementById('loading-screen');
+
+//     loadingScreen.classList.replace('hidden', 'flex');
+// })
+
+
+loginBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const loadingScreen = document.getElementById('loading-screen');
+
+    loadingScreen.classList.replace('hidden', 'flex');
+});
+
+
+const notifAlert = document.querySelectorAll('.alert');
+notifAlert.forEach(alert => {
+    alert.addEventListener('animationend', () => {
+        alert.remove();
+    })  
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,31 @@ module.exports = {
       fontFamily : {
         inter : ['Inter', 'serif'],
         poppins: ['Poppins', 'serif'],
-      }
+      },
+      keyframes : {
+        "fade-in": {
+              "0%": {
+                  opacity: 0
+              },
+              "100%": {
+                  opacity: 1
+              },
+          },
+          "fade-out": {
+              "0%": {
+                  opacity: 1
+              },
+              "100%": {
+                  opacity: 0
+              },
+          },
+      },
+
     },
+    animation : {
+      fadein: 'fade-in 1s ease-in-out 0.25s 1',
+      fadeoutTen: 'fade-out 1s ease-out 10s 1',
+    }
   },
   daisyui: {
     themes: [
